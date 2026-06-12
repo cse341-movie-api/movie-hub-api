3442de10816ee72abd032ea3929ff97e834eb65a
colebeebe <bee14003@byui.edu>
2026-06-12 00:07:06 -0400

    Created GET for all movies and single movie

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,1 +1,40 @@
-// Movies Controller
+const { getDb } = require('../db/connect');
+const { ObjectId } = require('mongodb');
+
+/**
+ * @description Retrieve all movies
+ * @route GET /movies
+ */
+const getAllMovies = async (req, res) => {
+  try {
+    const db = getDb();
+    const collection = db.collection('movies');
+    const movies = await collection.find({}).toArray();
+
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
+
+/**
+ * @description Retrieve a single movie using an id
+ * @route GET /movies/:id
+ */
+const getOneMovie = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const db = getDb();
+    const collection = db.collection('movies');
+    const movie = await collection.findOne(new ObjectId(id));
+
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  getAllMovies,
+  getOneMovie,
+};
--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/moviesController');
 
-// Code goes here
-
+router.get('/', controller.getAllMovies);
+router.get('/:id', controller.getOneMovie);
 
 module.exports = router;
 
