d18b34d2576f40c59f71c30aac4243d20a6fedcb
Eli Hansen <elihansen@yahoo.com>
2026-06-15 17:14:32 -0600

    Added usersRoutes.js and usersController.js

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/usersController');
 
-// Code goes here
+
+router.get('/', /* #swagger.tags = ['Users']*/ controller.getAllUsers);
+router.get('/:id', /* #swagger.tags = ['Users']*/ controller.getOneUser);
+router.post('/', /* #swagger.tags = ['Users']*/ controller.createUser);
+router.put('/:id', /* #swagger.tags = ['Users']*/ controller.updateUser);
+router.delete('/:id', /* #swagger.tags = ['Users']*/ controller.deleteUser);
 
 
 module.exports = router;
