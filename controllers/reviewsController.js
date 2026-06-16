381fd88a4ddbc2d59b1c8effc827b83354f99166
omarlinp <omarlinp@gmail.com>
2026-06-16 18:19:45 -0400

    CRUD operations completed

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -33,9 +33,83 @@ const getOneReview = async (req,res) =>{
     }
 }
 
+const postReview = async (req,res) => {
+    const review = {
+        userId : new ObjectId(req.body.userId),
+        movieId :new ObjectId(req.body.movieId),
+        rating : req.body.rating,
+        reviewText: req.body.reviewText, 
+        dateCreated:  new Date(), 
+        authorName: req.body.authorName, 
+        isSpoiler: req.body.isSpoiler
+    }
+    try {
+        const POST = await mongodb
+        .getDb()
+        .collection(collection)
+        .insertOne(review);
+    
+    if (POST.acknowledged) {
+                res.status(201).json(POST);
+                return;
+            }
+
+    } catch (error) {
+        res.status(500).json(error)
+        console.log(error)
+        return;
+    }
+}
+const updateReview = async (req,res) => {
+    const id = new ObjectId(req.params.id);
+    const review = {
+        userId : new ObjectId(req.body.userId),
+        movieId :new ObjectId(req.body.movieId),
+        rating : req.body.rating,
+        reviewText: req.body.reviewText, 
+        dateCreated:  new Date(), 
+        authorName: req.body.authorName, 
+        isSpoiler: req.body.isSpoiler 
+    };
+
+    try {
+        const PUT = await mongodb
+            .getDb()
+            .collection(collection)
+            .replaceOne({_id:id},review);
+        
+        if (PUT.acknowledged) {
+            return res.status(204).json(PUT);
+
+        }
+    } catch (error) {
+        return res.status(500).json(error);
+        
+    }
+}
+const deleteReview = async (req,res) => {
+    const id = new ObjectId(req.params.id);
+    try {
+        const DELETE = await mongodb
+            .getDb()
+            .collection(collection)
+            .deleteOne({ _id: id });
+        
+        if (DELETE.acknowledged) {
+            return res.status(204).json(DELETE);
+        }
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+
 module.exports = {
     getReviews,
-    getOneReview
+    getOneReview,
+    postReview,
+    updateReview,
+    deleteReview
 }
 
 /* */
--- routes/reviewsRoute.js
+++ routes/reviewsRoute.js
@@ -4,4 +4,7 @@ const controller = require('../controllers/reviewsController');
 // Code goes here
 router.get('/',controller.getReviews);
 router.get('/:id',controller.getOneReview);
+router.post('/',controller.postReview);
+router.put('/:id',controller.updateReview)
+router.delete('/:id',controller.deleteReview)
 module.exports = router;
