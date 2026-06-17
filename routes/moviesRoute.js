dd21dcbbd5a1f0487b9dd5cb3f36734aec706433
omarlinp <omarlinp@gmail.com>
2026-06-16 21:33:26 -0400

    created new routes

--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -5,6 +5,9 @@ const controller = require('../controllers/moviesController');
 router.get('/', controller.getAllMovies);
 router.get('/:id', controller.getOneMovie);
 router.post('/',controller.createMovie);
+router.put('/:id',controller.updateMovie);
+router.delete('/:id',controller.deteleMovie);
+
 
 module.exports = router;
 
