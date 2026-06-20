2c2bd3f1fc75e33f0946e1862502384b8096e75c
omarlinp <omarlinp@gmail.com>
2026-06-19 21:45:23 -0400

    added the swagger tags

--- routes/reviewsRoute.js
+++ routes/reviewsRoute.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/reviewsController');
 // Code goes here
-router.get('/',controller.getReviews);
-router.get('/:id',controller.getOneReview);
-router.post('/',controller.postReview);
-router.put('/:id',controller.updateReview)
-router.delete('/:id',controller.deleteReview)
+router.get('/',/*swagger.tags = ['Reviews']*/ controller.getReviews);
+router.get('/:id',/*swagger.tags = ['Reviews']*/ controller.getOneReview);
+router.post('/',/*swagger.tags = ['Reviews']*/ controller.postReview);
+router.put('/:id',/*swagger.tags = ['Reviews']*/ controller.updateReview)
+router.delete('/:id',/*swagger.tags = ['Reviews']*/ controller.deleteReview)
 module.exports = router;
