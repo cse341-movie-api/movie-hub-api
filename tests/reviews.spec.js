16513d14ee6c098f396401889ee77a80eff02dd4
omarlinp <omarlinp@gmail.com>
2026-07-08 10:39:17 -0400

    added the routes for testing

--- tests/reviews.spec.js
+++ tests/reviews.spec.js
@@ -15,3 +15,12 @@ const {
 // Initializing standalone Express application specifically for testing
 const app = express();
 app.use(express.json());
+
+// Mapping controller functions to matching endpoints
+app.get('/',getReviews);
+app.get('/:id', getOneReview);
+app.get('/user/:userid', userReview);
+app.get('/movie/:movieid', movieReview);
+app.post('/', createReview);
+app.put('/:id', updateReview);
+app.delete('/:id', deleteReview);
