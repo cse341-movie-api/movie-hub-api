5691c904e647015a21fe04a5619db99071fd76ba
omarlinp <omarlinp@gmail.com>
2026-07-09 09:21:46 -0400

    removed the mongodb package

--- tests/reviews.spec.js
+++ tests/reviews.spec.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
 const express = require('express');
 const getDb = require('../db/connect');
-const collection = require('mongodb');
 const {
     getReviews,
     getOneReview,
